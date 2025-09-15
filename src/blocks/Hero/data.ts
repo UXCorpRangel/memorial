418fec275b4358a83fe8fc436e5870967d655e09
ivanglpz <miltonivan117@gmail.com>
2025-09-15 10:40:35 -0600

    feat: hero images with stars

--- src/blocks/Hero/data.ts
+++ src/blocks/Hero/data.ts
@@ -8,18 +8,21 @@ export const heroData = {
       alt: "Imagen memorial 1",
       title: "Jaime Gutierrez 1993-2025",
       position: "lg:bottom-0 lg:translate-x-1/2 lg:-rotate-[8deg] lg:z-20",
+      showStars: false,
     },
     {
       src: "/Tyto-sandy-portrait.webp",
       alt: "Imagen memorial 2",
       title: "Miembro de UX Corp Rangel",
       position: "lg:-top-10 lg:translate-x-1/4 lg:-rotate-[8deg]",
+      showStars: false,
     },
     {
       src: "/Sandy-portrait.webp",
       alt: "Imagen memorial 3",
       title: "Sandy aprobadora de PR’s",
       position: "lg:right-0 lg:-translate-y-1/2 lg:rotate-[18deg] lg:z-10",
+      showStars: true,
     },
   ],
 };
