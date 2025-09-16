4ac40ad01e43ce4a270ab37248326107bf801ae7
Luis Perez <luism1p3@hotmail.com>
2025-09-16 11:05:43 -0600

    feat(donation): update form action and field name for message API

---
+++ src/pages/api/message.ts
@@ -0,0 +1,23 @@
+import type { APIRoute } from "astro";
+import { supabase } from "../../lib/supabase";
+
+export const POST: APIRoute = async ({ request, redirect }) => {
+  const formData = await request.formData();
+  const fullName = formData.get("fullName")?.toString();
+  const message = formData.get("message")?.toString();
+
+  if (!message) {
+    return new Response("Message is required", { status: 400 });
+  }
+
+  const { data, error } = await supabase.from("messages").insert({
+    fullName: fullName,
+    message,
+  });
+
+  if (error) {
+    return new Response(error.message, { status: 500 });
+  }
+
+  return redirect("/");
+};
