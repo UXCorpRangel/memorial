2ea6a5df9322b2b90e13cc58b61e309b973552ef
ivanglpz <miltonivan117@gmail.com>
2025-09-18 10:00:10 -0600

    fix: countdown utc  date chile event

--- src/blocks/Countdown/data.ts
+++ src/blocks/Countdown/data.ts
@@ -2,5 +2,5 @@ export const countdownData = {
   title: "Únete al evento en vivo por Jaime",
   description:
     "La comunidad de UX Corp Rangel quiere invitarte a su primer evento online,donde\n distintos creadores de contenido, diseñadores y desarrolladores seunirán en una\n transmisión especial para compartir conocimiento yexperiencias. Este encuentro tiene un\n propósito muy especial apoyar a losfamiliares de Jaime en la recaudación de fondos que\n necesitan en estemomento tan difícil. Podrás aprender, inspirarte y ser parte de una\n causasolidaria. Si no puedes donar, ¡compartir el evento ya es una gran ayuda!",
-  target_date: "2025-09-20T12:00:00",
+  target_date: "2025-09-20T17:30:00.000Z",
 };
